4260484222d06a56774ecc6a64270dd5bee1d7d9
rsyh97 <harry.rsyh@gmail.com>
2023-03-08 08:52:28 +0700

    first commit

---
+++ src/main/java/com/example/workshop/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
